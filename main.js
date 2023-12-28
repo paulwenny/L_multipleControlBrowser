44058d71e32146fdbde5cd67a2f72811b32b7964
paulwenny <pollywenny@gmail.com>
2023-12-29 06:38:32 +0800

    feature/assignment 1. index.html < menambahkan 2 div baru dengan id circe 2 dan circle 3 2. style.css < menambahkan style pada id circle 2 dan 3 dengan warna yang berbeda 3. main.js < mengelola id circle 2 dan 3 agar dapat di control 4. main.js < pada fungtion circleMovment posisi sumbu  x pada circle 2 + 25 dan circle 3 + 50 agar posisi tidak sejajar ketika di gerakkan di browser

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 const circleChannel = new BroadcastChannel("circle_channel")
-const circle = document.getElementById("circle")
+const circle1 = document.getElementById("circle1")
+const circle2 = document.getElementById("circle2");
+const circle3 = document.getElementById("circle3");
 
 function circleMovement(event){
     const x = event.clientX, y = event.clientY
-    circle.style.left = x + "px";
-    circle.style.right = "";
-    circle.style.top = y + "px";
+    // 01 blue
+    circle1.style.left = x + "px";
+    circle1.style.right = "";
+    circle1.style.top = y + "px";
+
+    // 02 orange
+    circle2.style.left = (x + 25 )+ "px";
+    circle2.style.right = "";
+    circle2.style.top = y + "px";
+
+    // 03 red
+    circle3.style.left = (x + 50) + "px";
+    circle3.style.right = "";
+    circle3.style.top = y + "px";
 
     circleChannel.postMessage({x , y})
 }
 
 circleChannel.onmessage = (event) => {
     const {x , y} = event.data
-    circle.style.right = x + "px";
-    circle.style.left = "";
-    circle.style.top = y + "px";
+    circle1.style.right = x + "px";
+    circle1.style.left = "";
+    circle1.style.top = y + "px";
+
+    circle2.style.right = (x + 25) + "px";
+    circle2.style.left = "";
+    circle2.style.top = y + "px";
+
+    circle3.style.right = (x + 50) + "px";
+    circle3.style.left = "";
+    circle3.style.top = y + "px";
 }
 
 addEventListener("mousemove" , circleMovement)
